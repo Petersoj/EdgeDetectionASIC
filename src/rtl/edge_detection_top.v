0b0101aebb5d09d841511ec62919d5357f44d97f
Jacob Peterson <net@jacobpeterson.net>
2020-10-01 13:24:33 -0600

    Add initial file structure

---
+++ src/rtl/edge_detection_top.v
@@ -0,0 +1,14 @@
+////
+//
+// University of Utah ECE 5710/6710 Edge Detection ASIC
+//
+// Create Date: 10/01/2020
+// Module Name: edge_detection_top
+// Description: The top module
+// Authors: Jacob Peterson, Daryl Warner, Trent Bennet, Aaron Pettit
+//
+////
+
+module edge_detection_top();
+
+endmodule
