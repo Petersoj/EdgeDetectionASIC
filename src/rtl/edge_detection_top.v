ffbf41b9a98a9d667dd35012b76ed67fb46552ae
DESKTOP-E31A0CT\Daryl <daryl.warner@gmail.com>
2020-10-09 14:46:20 -0600

    Add skeleton code (with I/O defined) for top level rtl and tb

--- src/rtl/edge_detection_top.v
+++ src/rtl/edge_detection_top.v
@@ -9,6 +9,26 @@
 //
 ////
 
-module edge_detection_top();
+module edge_detection_top
+  (
+    input wire          I_RST,       // Input Reset
+    input wire          I_CLK_100,   // Input Core Clock (100 MHz?)
+                                     
+    input wire  [23:0]  I_PIX_DATA,  // Input RGB Pixel Data
+    input wire	        I_VSYNC,     // Input Vertical Sync
+    input wire          I_HSYNC,     // Input Horizontal Sync
+    input wire          I_DE,        // Input Data Enable (Data Valid)
+    input wire          I_PCLK,      // Input Pixel Clock (25.175 MHz)
+                                     
+    output wire [23:0]  O_PIX_DATA,  // Output RGB Pixel Data
+    output wire         O_VSYNC,     // Output Vertical Sync
+    output wire         O_HSYNC,     // Output Horizontal Sync
+    output wire         O_DE,        // Output Data Enable (Data Valid)
+    output wire         O_PCLK       // Output Pixel Clock (25.175 MHz)
+  );
 
+  // Colorspace Converter
+  // Line Buffer
+  // Edge Detection
+  // Video Sync and Timing Gen
 endmodule
--- src/tb/edge_detection_top_tb.v
+++ src/tb/edge_detection_top_tb.v
@@ -9,6 +9,53 @@
 //
 ////
 
+`timescale 1 ps / 1 ps
+
 module edge_detection_top_tb();
+
+  reg   reset;
+  reg   clk_100;
+  wire  istrb_clk_100;
+
+  // generate clock
+  initial clk_100 = 1'b1;
+  always
+  begin
+    #5000 clk_100 = 1'b0;
+    #5000 clk_100 = 1'b1;
+  end
+  assign #300 istrb_clk_100 = clk_50;
+
+edge_detection_top DUT
+  (
+  .I_RST          (reset),     // Input Reset
+  .I_CLK_100      (clk_100),     // Input Core Clock (100 MHz?)
+  .I_PIX_DATA     (24'h0),     // Input RGB Pixel Data
+  .I_VSYNC        (1'b0),     // Input Vertical Sync
+  .I_HSYNC        (1'b0),     // Input Horizontal Sync
+  .I_DE           (1'b0),     // Input Data Enable (Data Valid)
+  .I_PCLK         (1'b0),     // Input Pixel Clock (25.175 MHz)
+  .O_PIX_DATA     (),     // Output RGB Pixel Data
+  .O_VSYNC        (),     // Output Vertical Sync
+  .O_HSYNC        (),     // Output Horizontal Sync
+  .O_DE           (),     // Output Data Enable (Data Valid)
+  .O_PCLK         ()      // Output Pixel Clock (25.175 MHz)
+  );
+
+  initial begin
+    $display("================================================================");
+    $display("========================== BEGIN SIM ===========================");
+    $display("================================================================");
+    reset = 1'b0;
+    repeat(10) @(posedge clk_100);
+    reset = 1'b1;
+    repeat(10) @(posedge clk_100);
+
+    // run tests
+
+    $display("================================================================");
+    $display("=========================== END SIM ============================");
+    $display("================================================================");
+  end
 
 endmodule
