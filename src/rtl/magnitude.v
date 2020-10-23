5930c84b0514c58f80dd17cb767d4f4d53909b8f
Aaron <aaron.pettit@comcast.net>
2020-10-23 16:52:45 -0600

    Created MSB module for magnitude computation. Modified input parameters for the sobel filter to include an input array. Cleaned up formating.

--- src/rtl/magnitude.v
+++ src/rtl/magnitude.v
@@ -1,30 +1,100 @@
 //magnitude of two integers using Newton's Method.
-//sqrt(val)~=a1=a0-(a0^2-val)/2*a0
+//sqrt(val)~=a_1=a_0-(a_0^2-val)/(2*a_0)
 //iterate 4 times
-//Uses integer division so there will be some error, but it shouldn't matter.
 //output max is 255
-module magnitude(a,b,out);
-	input signed[10:0] a,b; //max signed value 4*255=1020
-	output[7:0] out; //0-255 output\
-	reg[7:0] out;
-	reg [10:0] seed; //max 2032 value (2*1020^2)/(2^10)
-	wire [20:0] val; //max 21 bit value (signed 1020^2+1020^2)
-	reg [2:0] i;
+//inputs:
+//	a - 10 bit signed integer input
+//	b - 10 bit signed integer input
+//	start - flag to begin magnitude computation
+//outputs:
+//	out - output magnitude 0-255
+//	outValid - flag when complete
+module magnitude(a,b,start,out,outValid);
+	input signed[10:0] 	a; 
+	input signed[10:0] 	b; //max signed value 4*-255=-1020
+	input	wire		start;
+	output	reg[7:0]	out; //0-255 output
+	output 	reg			outValid; //1 when magnitude is complete
+	reg [10:0] 			seed; //max 2032 value (2*1020^2)/(2^10)
+	wire [20:0] 		val; //max 21 bit value (signed 1020^2+1020^2)
+	reg 				beginMSB; //1 to compute location of MSB
+	reg					resetMSB; //1 to reset MSB module. Required between each use
+	wire				validMSB; //1 when MSB is complete
+	reg [4:0] 			MSB_Index; //index of MSB
+	reg [2:0] 			i; //used in for loop
 
 	assign val=a*a+b*b; //a^2+b^2, need to check if valid with signed bits
-
-	always @(val) begin
-		if(val!=0) begin
-		//TODO calculate location of where MSB=1 and use Val/(2^(MSB/2)) to calculate the seed
-			seed=val/4; //could use case statement with different division factors to get closer to result.
-						//Larger seed values need to have higher division factors.
-						//possible division factor is 2^(n/2) where n is the msb thats a 1 (0010110 -> n=2)
-			//4 iterations 
+	positionOfMSB iMSB
+		(.data			(val),
+		 .start			(beginMSB),
+		 .reset			(resetMSB),
+		 .msbLocation	(MSB_Index),
+		 .msbValid		(validMSB)
+		 );
+		 
+	always @(posedge start) begin
+		resetMSB <= 0;
+		beginMSB <= 1;
+		outValid <= 0;
+	end
+	
+	always @(posedge validMSB) begin
+	
+		resetMSB = 1;
+		beginMSB = 0;
+		if(val==0) begin
+			out = 8'b0;
+		end
+		else begin
+			//Set initial seed to an estimated magnitude value. Divide original value by 2^(MSB_Index/2) where MSB_Index is highest bit that is a 1. This calculation will get the seed close enough for minimal interations
+			MSB_Index = MSB_Index / 2;
+			seed=val >> MSB_Index; 
+			//4 iterations needed to get within a few pixel values
 			for(i=0;i<4;i=i+1)begin
-				seed=seed-(seed*seed-val)/(2*seed); //integer division
+				seed=seed-(seed*seed-val)/(2*seed); //Newton's Method
 			end
 			if(seed[10:8]>0) out=8'd255; //magnitude > 255
 			else out = seed[7:0];
+		end
+		outValid = 1;
+		
+	end
+endmodule
+
+//Calculates the location of the MSB that is a 1 in the given data.
+//inputs:
+//	data - 21 bit integer
+//	start - flag to start the computation on posedge
+//	reset - flag to reset location and valid flag. Reset is required before next use.
+//outputs:
+//	msbLocation - output 5 bit integer with the location of the MSB
+//	msbValid - flag when computation is finished
+module positionOfMSB(data,start,reset,msbLocation,msbValid);
+	input [20:0]		data;
+	input 				start;
+	input				reset;
+	output	reg[4:0] 	msbLocation; //location of the highest order 1.
+	output 	reg 		msbValid; //output flag when finished
+	integer i;
+	
+	always @(posedge start, posedge reset) begin
+		if (reset) begin
+			msbLocation <= 0;
+			msbValid <= 0;
+		end
+		else if (start) begin
+			//exit if data=0, otherwise compute MSB
+			if(data==20'b0) begin
+				msbValid <=1;
+			end
+			else begin	
+				for (i = 20; i >= 0; i = i - 1) begin
+					if (data[i] == 1) begin
+						msbLocation <= i;
+						msbValid <= 1;
+					end
+				end
+			end
 		end
 	end
 endmodule
--- src/rtl/sobel.v
+++ src/rtl/sobel.v
@@ -4,7 +4,7 @@
 //	  col - column number starting from the left at 0
 //	  clk - input clock. Pixel computed on rising edge
 //output: out - filtered pixel at given [row,col]	  
-module Sobel(row,col,clk,out);
+/*module Sobel(row,col,clk,out);
 	input [9:0] row, col;
 	input clk;
 	output [7:0] out; //0-255 edge value
@@ -14,8 +14,8 @@ module Sobel(row,col,clk,out);
 	reg[7:0] val; //0-255 grey value
 	reg memoryReady;
 	wire[15:0] magnitudeVal; //sqrt value. Find out max number of bits needed for this.
-	parameter maxRow = 480;
-	parameter maxCol = 640;
+	parameter MAX_ROW = 480;
+	parameter MAX_COL = 640;
 
 	//flipped sobel filters
 	//	  -1 0 1	  -1 -2 -1
@@ -46,20 +46,20 @@ module Sobel(row,col,clk,out);
 				gy=gy-1*val;
 			end
 			gx=gx-2*inputPixel[row][col-1]; //row 2 col 1
-			if(row<maxRow-1)begin //row 3 col 1
+			if(row<MAX_ROW-1)begin //row 3 col 1
 				val=inputPixel[row+1][col-1];
 				gx=gx-1*val;
 				gy=gy+1*val;
 			end
 		end 
-		else if(col<maxCol-1) begin //col 3 valid
+		else if(col<MAX_COL-1) begin //col 3 valid
 			if(row>0) begin //row 1 col 3
 				val=inputPixel[row-1][col+1];
 				gx=gx+1*val;
 				gy=gy-1*val;
 			end
 			gx=gx+2*inputPixel[row][col+1]; //row 2 col 3
-			if(row<maxRow-1) begin //row 3 col 3
+			if(row<MAX_ROW-1) begin //row 3 col 3
 				val=inputPixel[row+1][col+1];
 				gx=gx+1*val;
 				gy=gy+1*val;
@@ -67,9 +67,9 @@ module Sobel(row,col,clk,out);
 		end
 		
 		if(row>0) gy=gy-2*inputPixel[row-1][col];//row 1 col 2
-		if(row<maxRow-1) gy=gy-2*inputPixel[row+1][col]; //row 3 col 2
+		if(row<MAX_ROW-1) gy=gy-2*inputPixel[row+1][col]; //row 3 col 2
 		
-		magnitude(gx,gy,magnitudeVal);
+		magnitude(gx,gy,magnitudeVal); //TODO fix declaration due to new arguments for magnitude
 	end
 
 	//magnitude has been computed.
@@ -84,81 +84,109 @@ module Sobel(row,col,clk,out);
 	end
 
 endmodule
-
+*/
 
 //Compute the sobel filter at given pixel location
 //alternative sobel filter without bound checking and just makes the border black
-//inputs: row - row number starting from the top at 0
-//	  col - column number starting from the left at 0
-//	  clk - input clock. Pixel computed on rising edge
-//output: out - filtered pixel at given [row,col]
-module sobel_blackBorder(row,col,clk,out);
-	input [9:0] row, col;
-	input clk;
-	output [7:0] out; //0-255 edge value
-	reg [7:0] out;
-	reg [7:0] inputPixels[8:0]; //pixel values in the order row1(col1-3),row2(col1-3),row3(col1-3)
-	wire[7:0] val; //0-255 grey value
-	reg signed [10:0] gx,gy; //range of gx,gy [-1020 : 1020]
-	wire[7:0] magnitudeVal; //value of the sqrt capped at 255
-	wire onEdge;
-	reg memoryReady;
-	parameter maxRow = 480;
-	parameter maxCol = 640;
-
-	//determine if center pixel is on the border
-	assign onEdge = row==0 || row==maxRow-1 || col==0 || col==maxCol-1;
-
-	//get inputPixels[] from memory, could get from memory at posedge of clk, then a ready signal for another always@ block
-
+//inputs:
+//	row - row number starting from the top at 0
+//	col - column number starting from the left at 0
+//	inputPixels - 64 bit array holding the 3x3 input pixels. Must be passed in as one large array with the 8 bit pixels combined.
+//		format: {top_left,top,top_right,mid_left,mid_right,bot_left,bot,bot_right}
+//		
+//	clk - input clock. Pixel computed on rising edge
+//	reset - set to high to reset internal flags. Required before next clock cycle.
+//outputs:
+//	out - filtered pixel at given [row,col]
+//	sobelFinished - set to 1 when the output is ready to be read
+module sobel_blackBorder(row,col,inputPixels,clk,reset,out,sobelFinished);
+	input 	wire [9:0] 	row;			//row number of output pixel
+	input	wire [9:0]	col;			//column number of output pixel
+	input 	wire [63:0] inputPixels; 	//Surrounding 8 bit pixel values combined. 
+										//format:
+										//	{top_left, top, top_right,
+										//	 mid_left, 		mid_right,
+										//	 bot_left, bot,	bot_right}
+	input 	wire		clk;			//pixel clock (25.175 MHz)
+	input	wire		reset;			//set to 1 to reset internal variables		
+	output 	reg	[7:0]	out;		 	//0,255 sobel pixel value
+	output	reg			sobelFinished;	//set to 1 when pixel is complete
+	
+	//variables
+	reg signed [10:0]	gx;				//output of horizontal filter range:[-1020:1020]
+	reg signed [10:0]	gy	;		 	//output of vertical filter range:[-1020 : 1020]
+	wire [7:0]			magnitudeVal;	//value of the sqrt capped at 255
+	wire [7:0]			pixelArray [7:0]; //array used to separate input pixel values
+	
+	//flags
+	wire 				onEdge;			//1 if the output pixel is on the edge of the image
+	reg					startMag;		//1 to compute the magnitude
+	wire				magFinished;	//1 when magnitdue has been computed
+	
+	//parameters
+	parameter [8:0]		MAX_ROW = 480;	//horizontal image dimension
+	parameter [9:0]		MAX_COL = 640;	//vertical image dimension
+	
 	//flipped sobel filters
 	//	  -1 0 1	  -1 -2 -1
 	//hx= -2 0 2  hy=  0  0  0
 	//	  -1 0 1	   1  2  1
+	parameter 			HX_11 	= -1;
+	parameter			HX_13	= 1;
+	parameter			HX_21	= -2;
+	parameter			HX_23	= 2;
+	parameter			HX_31	= -1;
+	parameter			HX_33	= 1;
+	
+	parameter			HY_11	= -1;
+	parameter			HY_12	= -2;
+	parameter			HY_13	= -1;
+	parameter			HY_31	= 1;
+	parameter			HY_32	= 2;
+	parameter			HY_33	= 1;
+	
 
+	//compute if center pixel is on the border
+	assign onEdge = (row==0) || (row==MAX_ROW-1) || (col==0) || (col==MAX_COL-1);
+	assign {pixelArray[7],pixelArray[6],pixelArray[5],pixelArray[4],pixelArray[3],pixelArray[2],pixelArray[1],pixelArray[0]} = inputPixels;
+	magnitdue Mag
+		(.a			(gx),
+		 .b			(gy),
+		 .start 	(startMag),
+		 .out		(magnitudeVal),
+		 .outValid	(magFinished)
+		 );
+		 
+	always @(posedge reset) begin
+		startMag<=0;
+		sobelFinished<=0;
+	end
+		 
+	//calculate horizontal and vertical filters
 	always @(posedge clk)begin
 		if(onEdge) begin
-			out = 8'd0;
+			out <= 8'd0;
+			sobelFinished<=1;
 		end
 		else begin
-			memoryReady=0;
-			//TODO get inputPixels[] from memory
-		
-			memoryReady=1;
+			gx <= HX_11*pixelArray[0] + HX_13*pixelArray[2] 
+				+ HX_21*pixelArray[3] + HX_23*pixelArray[4] 
+				+ HX_31*pixelArray[5] + HX_33*pixelArray[7];
+			gy <= HY_11*pixelArray[0] + HY_12*pixelArray[1] + HY_13*pixelArray[2] 
+				+ HY_31*pixelArray[5] + HY_32*pixelArray[6]	+ HY_33*pixelArray[7];
+			startMag<=1;
 		end
 	end
-	
-	always @(posedge memoryReady) begin
-			/* gx11 <= -1*inputPixels[0];	//row 1
-			gy11 <= -1*inputPixels[0];
-			gy12 <= -2*inputPixels[1];
-			gx13 <= 1*inputPixels[2];
-			gy13 <= -1*inputPixels[2];
-			gx21 <= -2*inputPixels[3];		//row 2
-			gx23 <= 2*inputPixels[5];
-			gx31 <= -1*inputPixels[6];		//row 3
-			gy31 <= inputPixels[6];
-			gy32 <= 2*inputPixels[7];
-			gx33 <= inputPixels[8];
-			gy33 <= inputPixels[9];
-			//can this be done in parallel?
-			gx <= gx11 + gx13 + gx21 + gx23 + gx31 + gx33;
-			gy <= gy11 + gy12 + gy13 + gy31 + gy32 + gy33; //is this valid when using "<=" in the same block as computing gy11?
-			*/
-			gx <= -inputPixels[0] + inputPixels[2] - 2*inputPixels[3] + 2*inputPixels[5] - inputPixels[6] + inputPixels[8];
-			gy <= -inputPixels[0] - 2*inputPixels[1] - inputPixels[2] + inputPixels[6] + 2*inputPixels[7] + inputPixels[8];
-			magnitude(gx,gy,magnitudeVal);
-	
-	end 
 
-	//magnitude has been computed.
-	always @(magnitudeVal) begin
+	//magnitude has been computed and output is updated
+	always @(posedge magFinished) begin
+		sobelFinished<=1;
 		//uses a threshold of 128. This helps to remove noise where the filter resulted in a low non-zero value.
-		if(magnitudeVal[7]) begin //result is greater than 127, set output to white.
-			out=8'd255;
+		if(magnitudeVal[7]==1) begin //result is greater than 127, set output to white.
+			out<=8'd255;
 		end
 		else begin
-			out=8'd0;
+			out<=8'd0;
 		end
 	end
 endmodule
