6a6af2e613509d356f639b2de384a3f3275772ab
Jacob Peterson <Petersoj@users.noreply.github.com>
2020-10-23 00:24:42 -0600

    Add colorspace skeleton

---
+++ src/rtl/colorspace/colorspace_converter.v
@@ -0,0 +1,14 @@
+////
+//
+// University of Utah ECE 5710/6710 Edge Detection ASIC
+//
+// Create Date: 10/23/2020
+// Module Name: colorspace_converter
+// Description: Converts an RGB colorspace input to grayscale.
+// Authors: Jacob Peterson
+//
+////
+
+module colorspace_converter();
+
+endmodule
---
+++ src/rtl/colorspace/frame_buffer.v
@@ -0,0 +1,14 @@
+////
+//
+// University of Utah ECE 5710/6710 Edge Detection ASIC
+//
+// Create Date: 10/23/2020
+// Module Name: frame_buffer
+// Description: A memory buffer for a video frame.
+// Authors: Jacob Peterson
+//
+////
+
+module frame_buffer();
+
+endmodule
