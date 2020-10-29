6722cbe2b8a1298132c8befee944327657083d73
Jacob Peterson <Petersoj@users.noreply.github.com>
2020-10-28 21:22:14 -0600

    Add all submodules to top

--- src/rtl/edge_detection_top.v
+++ src/rtl/edge_detection_top.v
@@ -39,18 +39,12 @@ module edge_detection_top
       pix_vsync_dly <= 1'b0;
       //pix_hsync_dly <= 1'b0;
       //pix_de_dly    <= 1'b0;
-
     end
     else begin
       pix_data_dly  <= I_PIX_DATA; // Will be changed later
       pix_vsync_dly <= I_VSYNC;    // Will be changed later
     end
   end
-
-  // Colorspace Converter
-  // Line Buffer
-  // Edge Detection
-  // Video Sync and Timing Gen
 
   parameter [11:0] VGA_HACT   = 640; // Horizontal Active (pixels)
   parameter [11:0] VGA_HFP    = 16;  // Horizontal Front Porch (pixels)
@@ -60,6 +54,61 @@ module edge_detection_top
   parameter [11:0] VGA_VFP    = 10;  // Vertical Front Porch (lines)
   parameter [11:0] VGA_VSW    = 2;   // Vertical Sync Width (lines)
   parameter [11:0] VGA_VBP    = 33;  // Vertical Back Porch (lines)
+
+  // Colorspace Converter
+  wire [$clog2(VGA_HACT) - 1:0] colorspace_converter_pixel_col;
+  wire [$clog2(VGA_VACT) - 1:0] colorspace_converter_pixel_row;
+  wire [23:0] colorspace_converter_pixel;
+  wire colorspace_converter_pixel_write_enable;
+
+  colorspace_converter iColorspaceConverter(
+      .I_CLK(I_CLK_100),
+      .I_RESET(I_RST),
+      .I_ENABLE(1'b1),
+
+      .I_PIX_DATA(I_PIX_DATA),
+      .I_VSYNC(I_VSYNC),
+      .I_HSYNC(I_HSYNC),
+      .I_DE(I_DE),
+      .I_PCLK(I_PCLK),
+
+      .O_PIXEL_COL(colorspace_converter_pixel_col),
+      .O_PIXEL_ROW(colorspace_converter_pixel_row),
+      .O_PIXEL(colorspace_converter_pixel),
+      .O_PIXEL_WRITE_ENABLE(colorspace_converter_pixel_write_enable)
+      );
+
+  // Frame Buffer
+  wire [23:0] frame_buffer_pixel;
+
+  frame_buffer iFrameBuffer(
+      .I_CLK(I_CLK_100),
+      .I_RESET(I_RST),
+      .I_ENABLE(1'b1),
+      .I_PIXEL_COL(colorspace_converter_pixel_col),
+      .I_PIXEL_ROW(colorspace_converter_pixel_row),
+      .I_PIXEL(colorspace_converter_pixel),
+      .I_WRITE_ENABLE(colorspace_converter_pixel_write_enable),
+      .I_READ_ENABLE(), // TODO
+
+      .O_PIXEL(frame_buffer_pixel)
+      );
+
+  // Edge Detection
+  wire [7:0] out;
+
+  sobel_blackBorder iSobelBlackBorder(
+      .row(colorspace_converter_pixel_row),
+      .col(colorspace_converter_pixel_col),
+      .inputPixels(), // TODO
+      .clk_pix(I_PCLK),
+      .clk(I_CLK_100),
+      .start(), // TODO
+      .out(out),
+      .done() // TODO
+      );
+
+  // Video Sync and Timing Gen
 
   parameter [11:0] VGA_HS_END   = VGA_HSW - 1;
   parameter [11:0] VGA_HBP_END  = VGA_HSW + VGA_HBP - 1;
@@ -74,7 +123,7 @@ module edge_detection_top
     (
       .I_RST      (I_RST),
       .I_PCLK     (I_PCLK),
-      .I_PIX_DATA (pix_data_dly), // Will need to be changed to Edge Detection Output
+      .I_PIX_DATA (out),
       .I_TP_EN    (1'b0),
       .I_HS_END   (VGA_HS_END),
       .I_HBP_END  (VGA_HBP_END),
